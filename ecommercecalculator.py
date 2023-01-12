02590193b0a4e93cadf5265e32f1422458924ede
Julia Ribeiro <112039292+iamjrbro@users.noreply.github.com>
2023-01-12 12:04:58 -0300

    Add files via upload

---
+++ ecommercecalculator.py
@@ -0,0 +1,14 @@
+'''
+ECOMMERCE CALCULATOR
+
+This code develops a program, using the While Loop Condition, that is able to calculate the commission that an ecommerce website charges to sell another person's product.
+
+    In this example, the hypothetical ecommerce has a 10% commission on top of the price of the product, if it has a value above $20.
+'''
+
+value = int(input('Please, type your product value:'))
+
+while value > 20:
+    value = (value * 0.10 + value)
+    print(f'Your products final price will be ${value}')
+    break
