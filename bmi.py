b0e14cc3a27852681e7ee97adbb2cafa54f396f1
Julia Ribeiro <112039292+iamjrbro@users.noreply.github.com>
2023-01-15 08:01:04 -0300

    Add files via upload

---
+++ bmi.py
@@ -0,0 +1,13 @@
+'''
+BMI'S CALCULATOR (Body Mass Index)
+
+This is a simplified version of a BMI's Calculator, with Ellipsis.
+'''
+
+name = 'Susi'
+height = 1.52
+weight = 50
+BMI = height / weight ** 2
+
+print(name, 'is', height, 'tall.')
+print('She weighs',  weight, 'kg and her BMI is', BMI)
